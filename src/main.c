8c47f84fc530698bc25974a08d973f9a19e4a840
Katharine Berry <katharine@kathar.in>
2013-05-17 06:15:30 -0700

    httpebble timezone demo

---
+++ src/main.c
@@ -0,0 +1,80 @@
+#include "pebble_os.h"
+#include "pebble_app.h"
+#include "pebble_fonts.h"
+
+#include "http.h"
+#include "itoa.h"
+
+PBL_APP_INFO(HTTP_UUID,
+             "Timezone Demo", "Your Company",
+             1, 0, /* App version */
+             DEFAULT_MENU_ICON,
+             APP_INFO_STANDARD_APP);
+
+Window window;
+TextLayer nameLayer;
+TextLayer offsetLayer;
+TextLayer dstLayer;
+TextLayer unixLayer;
+
+
+void have_time(int32_t dst_offset, bool is_dst, uint32_t unixtime, const char* tz_name) {
+	static char stz_name[50];
+	static char dst[] = "DST: yes";
+	static char dsto[] = "UTC offset: xxxxxxxxxxx";
+	static char unix[12];
+	text_layer_set_text(&unixLayer, "loaded");
+	if(!is_dst) memcpy(dst, "DST: no", 8);
+	text_layer_set_text(&dstLayer, dst);
+
+	memcpy(&dsto[12], itoa(dst_offset / 3600), 11);
+	text_layer_set_text(&offsetLayer, dsto);
+	
+	memcpy(&unix, itoa(unixtime), 11);
+	text_layer_set_text(&unixLayer, unix);
+	
+	memcpy(stz_name, tz_name, strlen(tz_name) + 1);
+	text_layer_set_text(&nameLayer, stz_name);
+}
+
+void failed(int32_t request_id, int http_status, void* context) {
+	text_layer_set_text(&unixLayer, itoa(http_status));
+}
+
+
+void handle_init(AppContextRef ctx) {
+	window_init(&window, "Window Name");
+	window_stack_push(&window, true /* Animated */);
+	
+	text_layer_init(&nameLayer, GRect(0, 0, 144, 30));
+	text_layer_init(&offsetLayer, GRect(0, 30, 144, 30));
+	text_layer_init(&dstLayer, GRect(0, 60, 144, 30));
+	text_layer_init(&unixLayer, GRect(0, 90, 144, 30));
+	layer_add_child(&window.layer, &nameLayer.layer);
+	layer_add_child(&window.layer, &offsetLayer.layer);
+	layer_add_child(&window.layer, &dstLayer.layer);
+	layer_add_child(&window.layer, &unixLayer.layer);
+	http_set_app_id(34525634);
+	http_register_callbacks((HTTPCallbacks){
+		.time=have_time,
+		.failure=failed
+	}, NULL);
+	HTTPResult result;
+
+	if((result = http_time_request()) != HTTP_OK) {
+		text_layer_set_text(&nameLayer, itoa(result));
+	}
+}
+
+void pbl_main(void *params) {
+	PebbleAppHandlers handlers = {
+		.init_handler = &handle_init,
+		.messaging_info = {
+			.buffer_sizes = {
+				.inbound = 256,
+				.outbound = 256,
+			}
+		},
+	};
+	app_event_loop(params, &handlers);
+}
